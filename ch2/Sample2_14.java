6afaa5dcb16a34cb43482e13db1f5ef15b35f119
112360113 <153254992+ntut112360113@users.noreply.github.com>
2025-11-09 16:35:12 +0800

    Update Sample2_14.java

--- ch2/Sample2_14.java
+++ ch2/Sample2_14.java
@@ -1,22 +1,13 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Sample2_14 {
     public static void main(String[] args) throws IOException {
-        System.out.println("要跳過第幾次的處理呢? (1~10)"); 
-        
-        BufferedReader br = new
-            BufferedReader(new InputStreamReader(System.in));
-        
-        String str = br.readLine();
-        int res = Integer.parseInt(str);
-        
+        System.out.print("要跳過第幾次的處理呢？(1~10)：");
+        int res = Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine());
+
         for (int i = 1; i <= 10; i++) {
-            if (i == res) { 
-                continue; 
-            }
-            System.out.println("第" + i + "次的處理。");
+            if (i == res) continue;
+            System.out.println("第 " + i + " 次的處理。");
         }
     }
 }
