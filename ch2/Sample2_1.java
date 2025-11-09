c25a5a7ebee84f30376d7a0fa2dd9e6caa7e7f8b
112360113 <153254992+ntut112360113@users.noreply.github.com>
2025-11-09 16:20:32 +0800

    Update Sample2_1.java

--- ch2/Sample2_1.java
+++ ch2/Sample2_1.java
@@ -1,22 +1,13 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Sample2_1 {
     public static void main(String[] args) throws IOException {
-        System.out.println("請輸入整數"); 
-        
-        BufferedReader br = 
-            new BufferedReader(new InputStreamReader(System.in)); 
-        
-        int num = Integer.parseInt(br.readLine()); 
-        
-        if (num == 1) { // 條件成立
-            System.out.println("輸入的是1");
-            System.out.println("選擇的是1");
-            
-        }
-        
+        System.out.print("請輸入整數：");
+        int num = Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine());
+
+        if (num == 1)
+            System.out.println("輸入的是1\n選擇的是1");
+
         System.out.println("結束處理");
     }
 }
