914a81392dce1ea07c5ea15e98fd81fb21b76126
112360113 <153254992+ntut112360113@users.noreply.github.com>
2025-11-09 17:37:36 +0800

    Create Sample3_24.java

---
+++ ch3/Sample3_24.java
@@ -0,0 +1,39 @@
+public class Sample3_24 {
+    public static void main(String[] args) {
+        Car[] cars = new Car[3];
+
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = new Car();
+        }
+
+        cars[0].setCar(1234, 20.5);
+        cars[1].setCar(2345, 30.5);
+        cars[2].setCar(3456, 40.5);
+
+        for (Car car : cars) {
+            car.show();
+        }
+    }
+}
+
+class Car {
+    private int num;
+    private double gas;
+
+    public Car() {
+        num = 0;
+        gas = 0.0;
+        System.out.println("生產了車子");
+    }
+
+    public void setCar(int n, double g) {
+        num = n;
+        gas = g;
+        System.out.println("將車號設為 " + num + ", 汽油量設為 " + gas);
+    }
+
+    public void show() {
+        System.out.println("車號是 " + num);
+        System.out.println("汽油量是 " + gas);
+    }
+}
