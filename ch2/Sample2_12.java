5ac7f429f4219c67df66f04308c9e65374f71e9d
112360113 <153254992+ntut112360113@users.noreply.github.com>
2025-11-09 16:33:59 +0800

    Update Sample2_12.java

--- ch2/Sample2_12.java
+++ ch2/Sample2_12.java
@@ -1,12 +1,7 @@
 public class Sample2_12 {
     public static void main(String[] args) {
-        int i, j;
-        
-        for (i = 0; i < 5; i++) { 
-            for (j = 0; j < 3; j++) { 
-                System.out.println("i圈" + i + "j是" + j);
-            }
-        }
-       
+        for (int i = 0; i < 5; i++)
+            for (int j = 0; j < 3; j++)
+                System.out.println("i圈 " + i + " j是 " + j);
     }
 }
