b3c060e8c16cb422df54f918ef4de0fef05b5125
112360113 <153254992+ntut112360113@users.noreply.github.com>
2025-11-09 15:29:12 +0800

    Update Sample2_4.java

--- ch2/Sample2_4.java
+++ ch2/Sample2_4.java
@@ -1,4 +1,6 @@
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Sample2_4 {
     public static void main(String[] args) throws IOException {
