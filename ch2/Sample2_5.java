3f6d098f52a8bc631858b6d53a8784a923b01464
112360113 <153254992+ntut112360113@users.noreply.github.com>
2025-11-09 16:27:50 +0800

    Update Sample2_5.java

--- ch2/Sample2_5.java
+++ ch2/Sample2_5.java
@@ -1,29 +1,14 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Sample2_5 {
     public static void main(String[] args) throws IOException {
-        System.out.println("請輸入整數"); 
-        
-        BufferedReader br = 
-            new BufferedReader(new InputStreamReader(System.in));
-        
-        int num = Integer.parseInt(br.readLine());
-        
+        System.out.print("請輸入整數：");
+        int num = Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine());
+
         switch (num) {
-            case 1: { 
-                System.out.println("輸入的是1");
-                break;
-            }
-            case 2: {
-                System.out.println("輸入的是2");
-                break;
-            }
-            default: { 
-                System.out.println("請輸入1或2");
-                break;
-            }
+            case 1 -> System.out.println("輸入的是1");
+            case 2 -> System.out.println("輸入的是2");
+            default -> System.out.println("請輸入1或2");
         }
     }
 }
