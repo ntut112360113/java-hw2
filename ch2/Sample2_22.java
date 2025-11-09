9143eba368299aea856390de92a70733f276a722
112360113 <153254992+ntut112360113@users.noreply.github.com>
2025-11-09 16:43:58 +0800

    Update Sample2_22.java

--- ch2/Sample2_22.java
+++ ch2/Sample2_22.java
@@ -1,22 +1,13 @@
 public class Sample2_22 {
     public static void main(String[] args) {
-        int[][] test;
-        test = new int[2][5]; 
-                
-        test[0][0] = 80;
-        test[0][1] = 60;
-        test[0][2] = 22;
-        test[0][3] = 50;
-        test[0][4] = 75;
-        
-        test[1][0] = 90;
-        test[1][1] = 55;
-        test[1][2] = 68;
-        test[1][3] = 72;
+        int[][] test = {
+            {80, 60, 22, 50, 75}, // 國語
+            {90, 55, 68, 72, 58}  // 數學
+        };
 
-                for (int i = 0; i < test[0].length; i++) { 
-            System.out.println("第" + (i + 1) + "個人的國語分數是" + test[0][i] + "分");
-            System.out.println("第" + (i + 1) + "個人的數學分數是" + test[1][i] + "分");
+        for (int i = 0; i < test[0].length; i++) {
+            System.out.println("第 " + (i + 1) + " 個人的國語分數是 " + test[0][i] + " 分");
+            System.out.println("第 " + (i + 1) + " 個人的數學分數是 " + test[1][i] + " 分");
         }
     }
 }
