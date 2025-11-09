fbc100e475f36f216e612af9d775a313155baab4
112360113 <153254992+ntut112360113@users.noreply.github.com>
2025-11-09 16:28:59 +0800

    Update Sample2_6.java

--- ch2/Sample2_6.java
+++ ch2/Sample2_6.java
@@ -1,30 +1,14 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Sample2_6 {
     public static void main(String[] args) throws IOException {
-        System.out.println("請輸入a或b"); 
-        
-        BufferedReader br = 
-            new BufferedReader(new InputStreamReader(System.in));
-        
-        String str = br.readLine();
-        char letter = str.charAt(0); 
-        
-        switch (letter) { 
-            case 'a': {
-                System.out.println("輸入的是a");
-                break;
-            }
-            case 'b': {
-                System.out.println("輸入的是b");
-                break;
-            }
-            default: {
-                System.out.println("請輸入a或b");
-                break;
-            }
+        System.out.print("請輸入 a 或 b：");
+        char letter = new BufferedReader(new InputStreamReader(System.in)).readLine().charAt(0);
+
+        switch (letter) {
+            case 'a' -> System.out.println("輸入的是 a");
+            case 'b' -> System.out.println("輸入的是 b");
+            default -> System.out.println("請輸入 a 或 b");
         }
     }
 }
