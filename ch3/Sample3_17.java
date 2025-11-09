07baf43464bae27206fffd9b3dadabead06fb74b
112360113 <153254992+ntut112360113@users.noreply.github.com>
2025-11-09 17:31:43 +0800

    Create Sample3_17.java

---
+++ ch3/Sample3_17.java
@@ -0,0 +1,9 @@
+public class Sample3_17 {
+    public static void main(String[] args) {
+        String str = "Hello";
+        
+        System.out.println(str + " 的第一個字元是 " + str.charAt(0));
+        System.out.println(str + " 的第二個字元是 " + str.charAt(1));
+        System.out.println(str + " 的長度是 " + str.length());
+    }
+}
