fca02753b970f5452ac27c2d99350ace80b7836d
112360113 <153254992+ntut112360113@users.noreply.github.com>
2025-11-09 16:30:54 +0800

    Update Sample2_8.java

--- ch2/Sample2_8.java
+++ ch2/Sample2_8.java
@@ -1,21 +1,11 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Sample2_8 {
     public static void main(String[] args) throws IOException {
-        System.out.println("請問要選哪條路線?"); 
-        System.out.println("請輸入整數"); 
-        
-        BufferedReader br = 
-            new BufferedReader(new InputStreamReader(System.in));
-        
-        String str = br.readLine();
-        int res = Integer.parseInt(str);
-        
-        
-        char ans = (res == 1) ? 'A' : 'B'; 
+        System.out.print("請問要選哪條路線？請輸入整數：");
+        int res = Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine());
 
-        System.out.println("選擇了" + ans + "路線");
+        char ans = (res == 1) ? 'A' : 'B';
+        System.out.println("選擇了 " + ans + " 路線");
     }
 }
